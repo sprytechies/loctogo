c2b0eb3cbf5e044a698ff7fe1296e5399e6ecca9
bhanuagarwal <bhanu.agarwal@sprytechies.com>
2014-09-11 17:59:35 +0530

    db configuration commit

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=loctogo',
             'username' => 'root',
-            'password' => '',
+            'password' => '1234',
             'charset' => 'utf8',
         ],
         'mailer' => [
