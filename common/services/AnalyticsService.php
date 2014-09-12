0729e8bbe8dc07ed8074f65935abd05d2fb4b23a
sprytechies <arvind@sprytechies.com>
2014-09-12 13:01:27 +0530

    activity push

---
+++ common/services/AnalyticsService.php
@@ -0,0 +1,19 @@
+<?php
+
+class AnalyticsService
+{
+    public function setUp()
+    {
+        // ... Set up environment for this job
+    }
+
+    public function perform()
+    {
+        // .. Run job
+    }
+
+    public function tearDown()
+    {
+        // ... Remove environment for this job
+    }
+}
