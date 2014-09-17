5945fd1e996cdd065acc67c6ecd160a7405e13b3
sprytechies <arvind@sprytechies.com>
2014-09-17 11:47:00 +0530

    Main file changes. Configure your database accordingly.

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=loctogo',
             'username' => 'root',
-            'password' => '1234',
+            'password' => '',
             'charset' => 'utf8',
         ],
         'mailer' => [
