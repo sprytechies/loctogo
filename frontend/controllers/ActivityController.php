d0c744c58d599c353aea6f51bcb21b9bb23ccb37
bhanuagarwal <bhanu.agarwal@sprytechies.com>
2014-09-22 15:12:47 +0530

    Activity screen layout

--- frontend/controllers/ActivityController.php
+++ frontend/controllers/ActivityController.php
@@ -1,14 +1,42 @@
 <?php
 
 namespace frontend\controllers;
-use frontend\components\Controller;
-class ActivityController extends Controller
+
+use common\models\Todo;
+use common\models\Comments;
+use Yii;
+use yii\db\ActiveRecord;
+use yii\db\Query;
+use yii\data\ArrayDataProvider;
+class ActivityController extends \yii\web\Controller
 {
     public $layout = 'column2';
     
-    public function actionIndex()
+    public function actionIndex($id)
+        {
+          $text= new Comments;
+          $sql="select description,cdate from todo where idtodo=".$id;
+          $model=  Todo::findBySql($sql)->one();
+          $query = new Query();
+          $provider = new ArrayDataProvider([
+          'allModels' => $query->from('comments')->all(),
+        ]);
+        return $this->render('index',['model'=>$model,'text'=>$text,'dataprovider'=>$provider]);
+        }
+        
+    public function actionComment()
     {
-        return $this->render('index');
+             $frm=new Comments;
+            if($frm->load(Yii::$app->request->post()))
+            {
+                $frm->idtodo = 1;
+                $frm->iduser = Yii::$app->user->id;
+                if($frm->save())
+                    echo "success";
+                else
+                    print_r($frm->getErrors());
+            }
+            
     }
 
 }
--- frontend/views/activity/index.php
+++ frontend/views/activity/index.php
@@ -1,9 +1,49 @@
 <?php
+ namespace frontend\views\activity;
 /* @var $this yii\web\View */
+ 
+ use yii\helpers\Html;
+ use yii\widgets\ActiveForm;
+ use yii\widgets\ListView;
 ?>
-<h3>Activity</h3>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+    <?php
+         echo "<h3>".$model->description."</h3>"."posted at ".$model->cdate;
+       ?>
+    <?php $form = ActiveForm::begin(['id' => 'comment-form']); ?>
+    <?= $form->field($text, 'description')->label('')->textarea(['rows'=>6,'placeholder'=>'Write your comment']);?>
+    
+    <?= Html::submitButton('Post Comment', ['class' => 'btn btn-primary green', 
+        'name' => 'comment-button','onClick'=>'comment(); return false;']) ?>
+    <?php ActiveForm::end(); ?>
+     <?php \yii\widgets\Pjax::begin(['id'=>'request']); ?>
+    <?php echo ListView::widget([
+         'dataProvider' => $dataprovider,
+         'itemOptions' => ['class' => 'list-item'],
+         'itemView' => '_form',
+         'layout' => '{items}{pager}',
+         'id'=>'list',
+    ]);
+     ?>
+    <?php \yii\widgets\Pjax::end(); ?>
+<script>
+            function comment(){
+                $.ajax({
+                        type:'POST',
+                        url:'index.php?r=activity/comment',
+                        data : $('#comment-form').serialize(),
+                        success: function(data)
+                        {
+                            $.pjax.reload({container:'#request'});
+                            $('textarea').filter('[id*=comments-description]').val('');
+                        }
+
+                       });
+                   return false;
+                }
+ </script>
+      
+
+   
+   
+ 
