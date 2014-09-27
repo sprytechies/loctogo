9ea03ef9d2f7d60aa3c216c26d834c2b1ece43fe
bhanuagarwal <bhanu.agarwal@sprytechies.com>
2014-09-27 09:35:35 +0530

    Activity screen layout

--- frontend/views/activity/index.php
+++ frontend/views/activity/index.php
@@ -5,17 +5,17 @@
  use yii\helpers\Html;
  use yii\widgets\ActiveForm;
  use yii\widgets\ListView;
-?>
-
+ 
+ ?>
     <?php
          echo "<h3>".$model->description."</h3>"."posted at ".$model->cdate;
        ?>
     <?php $form = ActiveForm::begin(['id' => 'comment-form']); ?>
     <?= $form->field($text, 'description')->label('')->textarea(['rows'=>6,'placeholder'=>'Write your comment']);?>
-    
     <?= Html::submitButton('Post Comment', ['class' => 'btn btn-primary green', 
         'name' => 'comment-button','onClick'=>'comment(); return false;']) ?>
     <?php ActiveForm::end(); ?>
+  
      <?php \yii\widgets\Pjax::begin(['id'=>'request']); ?>
     <?php echo ListView::widget([
          'dataProvider' => $dataprovider,
@@ -23,15 +23,18 @@
          'itemView' => '_form',
          'layout' => '{items}{pager}',
          'id'=>'list',
+        'viewParams'=>['text'=>$text],
+         
     ]);
-     ?>
+    ?>
     <?php \yii\widgets\Pjax::end(); ?>
+
 <script>
             function comment(){
                 $.ajax({
                         type:'POST',
                         url:'index.php?r=activity/comment',
-                        data : $('#comment-form').serialize(),
+                        data:$('#comment-form').serialize(),
                         success: function(data)
                         {
                             $.pjax.reload({container:'#request'});
@@ -40,10 +43,13 @@
 
                        });
                    return false;
+                   
                 }
- </script>
-      
+                
+</script>
 
+
+      
    
    
  
