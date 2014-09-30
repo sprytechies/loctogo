9ab38c4f439a6a4bb3fb92e3c508d52022c8603e
bhanuagarwal <bhanu.agarwal@sprytechies.com>
2014-09-30 15:17:02 +0530

    Dashboard template

--- frontend/views/dashboard/index.php
+++ frontend/views/dashboard/index.php
@@ -1,13 +1,141 @@
-<?php
-/* @var $this yii\web\View */
-?>
-<div class="row">
-    <div class="col-sm-3">
-        <div class="sidebar-module section-background">
-            <h4>About</h4>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-        </div>
+
+<div class="col-lg-8 col-md-8 col-sm-8">
+    <div class="top-dest">
+        <h4>Top Destinations</h4>
     </div>
-    <div class="col-sm-8 section-background">
+    <div class="destination">
+        <div class="destination-list">
+            <h4 class="proxima-semib">New York</h4>
+            <div class="list-panel">
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 2 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">Go to straight right of liberty</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 7 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">Gmail App (Android) - How to stop text from</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 0 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">Images on background images</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 1 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">What's Your Favorite Hack?</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 3 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">How to create an email gallery?</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+            </div>
+        </div>
+        <div class="showmore"> <a href="javascript:void(0)">Show more <span class="icon-plus2"></span></a> </div>
+        <div class="destination-list">
+            <h4 class="proxima-semib">Jaipur</h4>
+            <div class="list-panel">
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 2 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">Go to straight right of liberty</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 7 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">Gmail App (Android) - How to stop text from</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 0 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">Images on background images</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 1 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">What's Your Favorite Hack?</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+                <div id="discussion" class="post-summary">
+                    <div class="voting up"><img src="<?= Yii::$app->request->baseUrl; ?>/images/up-arrow.png" class="up-arrow"> 3 <span>Votes</span> </div>
+                    <div class="post-description">
+                        <h2><a href="javascript:void(0)">How to create an email gallery?</a></h2>
+                        <ul>
+                            <li><a href="javascript:void(0)"><span class="icon-star post-icon"></span>Favourite</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-plus post-icon"></span>Add to list</a></li>
+                            <li><a href="javascript:void(0)"><span class="icon-forward post-icon"></span>Reply</a></li>
+                        </ul>
+                    </div>
+                    <div class="voting down"><img src="<?= Yii::$app->request->baseUrl; ?>/images/down-arrow.png" class="down-arrow"> 0 <span>Votes</span> </div>
+                </div>
+            </div>
+        </div>
+        <div class="showmore"> <a href="javascript:void(0)">Show more <span class="icon-plus2"></span></a> </div>
     </div>
 </div>
+
